7828503d3ff47c9ec3fca3a24dc46e2db73578ac
Eric <cruze@oregonstate.edu>
2016-03-08 17:46:15 -0800

    Added Google Static Maps functionality for location pictures

--- app/src/main/java/com/example/eric/reuserepair/app/DetailActivity.java
+++ app/src/main/java/com/example/eric/reuserepair/app/DetailActivity.java
@@ -1,7 +1,10 @@
 package com.example.eric.reuserepair.app;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -14,16 +17,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.String;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.lang.String;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -71,6 +81,8 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     public static class DetailFragment extends Fragment {
+
+        ImageView mImg;
         ArrayAdapter<String> mDetailAdapter;
         private final String LOG_TAG = DetailFragment.class.getSimpleName();
 
@@ -81,11 +93,19 @@ public class DetailActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
 
+            /*
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            */
+
             int businessID = 0;
             ArrayList<String> data = new ArrayList<String>();
             String selectedBusiness = getActivity().getIntent().getExtras().getString("business");
             JSONArray businessRecords = null;
             JSONArray lookingForBID;
+            double lat = 0;
+            double lng = 0;
 
             Log.v(LOG_TAG, "Business string: " + selectedBusiness);
 
@@ -109,6 +129,8 @@ public class DetailActivity extends AppCompatActivity {
                         data.add(lookingForBID.getString(4));
                         data.add(lookingForBID.getString(5));
                         data.add(lookingForBID.getString(6));
+                        lat = lookingForBID.getDouble(7);
+                        lng = lookingForBID.getDouble(8);
                         break;
                     }
                 } catch (JSONException e) {
@@ -130,7 +152,59 @@ public class DetailActivity extends AppCompatActivity {
             ListView listView = (ListView) rootView.findViewById(R.id.listview_detail);
             listView.setAdapter(mDetailAdapter);
 
+            mImg = (ImageView) rootView.findViewById(R.id.static_map_view);
+            String url = "http://maps.google.com/maps/api/staticmap?center=&markers=color:blue%7Clabel:S%7C" + lat + "," + lng + "&zoom=15&size=300x150&maptype=roadmap&sensor=false";
+            GetMapsImage getMapsImage = new GetMapsImage();
+            Bitmap bm = null;
+            try {
+                bm = getMapsImage.execute(url).get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            mImg.setImageBitmap(bm);
+
+            //BitmapFactory.Options bmOptions;
+            //bmOptions = new BitmapFactory.Options();
+            //Bitmap bm = loadBitmap(url, bmOptions);
+
             return rootView;
         }
+
+        /*
+        public static Bitmap loadBitmap(String URL, BitmapFactory.Options options) {
+            Bitmap bitmap = null;
+            InputStream in = null;
+            try {
+                in = OpenHttpConnection(URL);
+                bitmap = BitmapFactory.decodeStream(in, null, options);
+                in.close();
+            } catch (IOException e1) {
+            }
+            return bitmap;
+        }
+        */
+
+        /*
+        private static InputStream OpenHttpConnection(String strURL)
+                throws IOException {
+            InputStream inputStream = null;
+            URL url = new URL(strURL);
+            URLConnection conn = url.openConnection();
+
+            try {
+                HttpURLConnection httpConn = (HttpURLConnection) conn;
+                httpConn.setRequestMethod("GET");
+                httpConn.connect();
+
+                if (httpConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpConn.getInputStream();
+                }
+            } catch (Exception ex) {
+            }
+            return inputStream;
+        }
+        */
     }
 }
---
+++ app/src/main/java/com/example/eric/reuserepair/app/GetMapsImage.java
@@ -0,0 +1,65 @@
+package com.example.eric.reuserepair.app;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.String;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Eric on 3/7/2016.
+ */
+public class GetMapsImage extends AsyncTask<String, Void, Bitmap> {
+
+    @Override
+    protected Bitmap doInBackground(String ...urls) {
+        String url = urls[0];
+        BitmapFactory.Options bmOptions;
+        bmOptions = new BitmapFactory.Options();
+
+        return loadBitmap(url, bmOptions);
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap result) {
+        super.onPostExecute(result);
+    }
+
+    public static Bitmap loadBitmap(String URL, BitmapFactory.Options options) {
+        Bitmap bitmap = null;
+        InputStream in;
+        try {
+            in = OpenHttpConnection(URL);
+            bitmap = BitmapFactory.decodeStream(in, null, options);
+            in.close();
+        } catch (IOException e1) {
+        }
+        return bitmap;
+    }
+
+    private static InputStream OpenHttpConnection(String strURL)
+            throws IOException {
+        InputStream inputStream = null;
+        URL url = new URL(strURL);
+        URLConnection conn = url.openConnection();
+
+        try {
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+
+            if (httpConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                inputStream = httpConn.getInputStream();
+            }
+        } catch (Exception ex) {
+        }
+        return inputStream;
+    }
+}
